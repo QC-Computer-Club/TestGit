a1d8faa6b12b754ad7a15f7e6362e72057f056f3
Instructor <Instructor>
2017-02-17 11:39:00 -0500

    first commit

---
+++ ImageApp.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.image.*;
+import javax.swing.*;
+
+public class ImageApp {  
+   public void loadAndDisplayImage(JFrame frame) {  
+      // Load the img  
+      BufferedImage loadImg = ImageUtil.loadImage("PAX-East-logo.gif");  
+      frame.setBounds(0, 0, loadImg.getWidth(), loadImg.getHeight()); 
+      System.out.printf("Width: %d, Height: %d%n",
+         loadImg.getWidth(), loadImg.getHeight());
+         
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      JImagePanel panel = new JImagePanel(loadImg, 0, 0);  
+      frame.add(panel); 
+        
+      // Set the panel visible and add it to the frame  
+      frame.setVisible(true); 
+   }  
+      
+   public static void main(String[] args) {  
+      ImageApp ia = new ImageApp();
+      JFrame frame = new JFrame("Tutorials");  
+      ia.loadAndDisplayImage(frame);  
+   }  
+}  
---
+++ ImageUtil.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.awt.image.*;
+import javax.imageio.*;
+
+public class ImageUtil
+{
+   public static BufferedImage loadImage(String ref) {  
+      BufferedImage bimg = null;  
+      try {  
+         bimg = ImageIO.read(new File(ref));  
+      } catch (Exception e) {  
+         e.printStackTrace();  
+      }  
+      return bimg;
+   }  
+}
